1f2498c45df2a5579c7451ef6abf28370a23b24c
akrem <akrem@NIKO>
2023-09-24 13:35:19 +0400

    Update 3.7 Add Partial navigation Add partial header Update design

--- IRepairman/wwwroot/js/site.js
+++
@@ -1,8 +0,0 @@
-﻿const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
-
